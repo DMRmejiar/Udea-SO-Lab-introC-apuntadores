0c1dd955607cfa2e07182e238cf23717ae0d292a
David Mejia Restrepo <dmrmejia57@gmail.com>
2020-03-07 18:10:49 -0500

    Add files Ejercicio 1 and 2

---
+++ codigos_seccion_5/e01.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void test(void);
+int contarCaracter(char *, char);
+
+int main() {
+  test();
+  return 0;
+}
+
+/**
+ *   @brief  Cuenta las veces que aparece un caracter determinado dentro de una cadena.
+ *
+ *   @param  array cadena de caracteres a ingresar
+ *   @param  ch es el caracter a averiguar
+ *   @return el numero de veces que aparece ch en array o -1 si no aparece.
+ */
+int contarCaracter(char *array, char ch) {
+  int cnt = 0;
+  while (*array != '\0') {
+    if(ch == *array) {
+      cnt++;
+    }
+    array++;
+  }
+  if (cnt == 0) {
+      cnt = -1;
+    }
+  return cnt;
+}
+
+void test() {
+  char cad[] = "cadena";
+  int cnt = contarCaracter(cad,'a');
+  printf("Contar a en \"%s\" : %d\n", cad, cnt);
+  cnt = contarCaracter(cad,'n');
+  printf("Contar n en \"%s\" : %d\n", cad, cnt);
+  cnt = contarCaracter(cad,'p');
+  printf("Contar p en \"%s\" : %d\n", cad, cnt);
+}
---
+++ codigos_seccion_5/e02.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void test(void);
+int contarCaracter(char *, char);
+
+int main() {
+  test();
+  return 0;
+}
+
+/**
+ *   @brief  Cuenta las veces que aparece un caracter determinado dentro de una cadena.
+ *
+ *   @param  array cadena de caracteres a ingresar
+ *   @param  ch es el caracter a averiguar
+ *   @return el numero de veces que aparece ch en array o -1 si no aparece.
+ */
+int obtenerIndice(char *array, char ch) {
+  int cnt = 0;
+  while (*array != '\0') {
+    if(ch == *array) {
+        return cnt;
+    }
+    cnt++;
+    array++;
+  }
+  return -1;
+}
+
+void test() {
+  char cad[] = "cadena";
+  int cnt = obtenerIndice(cad,'a');
+  printf("Posicion de a en \"%s\" : %d\n", cad, cnt);
+  cnt = obtenerIndice(cad,'n');
+  printf("Posicion de n en \"%s\" : %d\n", cad, cnt);
+  cnt = obtenerIndice(cad,'p');
+  printf("Posicion de p en \"%s\" : %d\n", cad, cnt);
+}
