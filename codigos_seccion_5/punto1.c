4765cf4911fdee30f762043c76340182920de745
David Mejia Restrepo <dmrmejia57@gmail.com>
2020-03-08 15:09:18 -0500

    Resolve questions

---
+++ codigos_seccion_5/punto1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Codigo tomado de: https://en.wikibooks.org/wiki/C_Programming/stdio.h/getchar
+
+ int main(void)
+ {
+   char str[1000];
+   int ch, n = 0;
+   while ((ch = getchar()) != EOF && n < 3) {
+      str[n] = ch;
+      ++n;
+   }
+   putchar('\n');
+   for (int i = 0; i < n; ++i)
+      putchar(str[i]);
+
+   putchar('\n');
+   return 0;
+ }
